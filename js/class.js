56f07e77334d3dde480f7c34d839c3a426fa0c91
Gary1q2 <gary1q2@hotmail.com>
2019-12-06 22:56:16 +1100

    Got rid of grid movement, everything is smoooth now -Player now have movespeed -Bullets now have movespeed

--- js/class.js
+++ js/class.js
@@ -7,10 +7,49 @@ class Entity {
 		this.img = img;
 	}
 	draw() {
-		ctx.drawImage(this.img,this.x*gridLen,this.y*gridLen);
+		ctx.drawImage(this.img,this.x,this.y);
 	}
 	update() {
 		this.draw();
+	}
+}
+
+class AnimEntity extends Entity {
+	constructor(x, y, width, height, img, nRow, nCol, frameSeq) {
+		super(x, y, width, height, img);
+
+		this.nRow = nRow;
+		this.nCol = nCol;
+		this.frameSeq = frameSeq;
+
+		this.animIndex = 0;
+		this.animCurrFrame = 0;
+		this.animDelay = 0;
+		this.finishAnim = false;
+	}
+	update() {
+		this.drawAnimated(this.frameSeq); 
+	}
+ 	drawAnimated(array) {
+		this.finishAnim = false;
+		this.animCurrFrame = array[this.animIndex];
+
+		var xPos = (this.animCurrFrame % this.nRow) * this.img.width/this.nCol;
+		var yPos = Math.floor(this.animCurrFrame / this.nCol) * this.img.height/this.nRow;
+
+		ctx.drawImage(this.img, xPos, yPos, this.img.width/this.nCol, this.img.height/this.nRow, this.x, this.y,
+					  this.img.width/this.nCol, this.img.height/this.nRow);
+
+		if (this.animDelay <= 4) {
+			this.animDelay++;
+		} else {
+			this.animDelay = 0;
+			this.animIndex++;
+			if (this.animIndex >= array.length) {
+				this.animIndex = 0;
+				this.finishAnim = true;
+			}
+		}
 	}
 }
 
@@ -18,63 +57,48 @@ class Entity {
 class Bullet extends Entity{
 	constructor(x, y, dir, width, height, img) {
 		super(x, y, width, height, img);
-
+		this.speed = 2;
 		this.dir = dir;
 		this.dead = false;
-
-		this.moveTime = 5;
-		this.moveTimer = 0;
 	}
+
 	update() {
-
-
 		this.updateMovement();
 		this.checkDead();
 		super.update();
 	}
 
 	updateMovement() {
-		if (this.moveTimer == 0) {
-			if (this.dir == "left") {
-				this.x--;
-				this.moveTimer = this.moveTime;
-			} else if (this.dir == "right") {
-				this.x++;
-				this.moveTimer = this.moveTime;
-			} else if (this.dir == "up") {
-				this.y--;
-				this.moveTimer = this.moveTime;
-			} else if (this.dir == "down") {
-				this.y++;
-				this.moveTimer = this.moveTime;
-			}
-		}
-
-		if (this.moveTimer > 0) {
-			this.moveTimer--;
-		}
+		if (this.dir == "left") {
+			this.x -= this.speed;
+			this.moveTimer = this.moveTime;
+		} else if (this.dir == "right") {
+			this.x += this.speed;
+			this.moveTimer = this.moveTime;
+		} else if (this.dir == "up") {
+			this.y -= this.speed;
+			this.moveTimer = this.moveTime;
+		} else if (this.dir == "down") {
+			this.y += this.speed;
+			this.moveTimer = this.moveTime;
+		}	
 	}
 
 	// Check if out of bounds or not
 	checkDead() {
-		if (this.x < 0 || this.x > numWidth-1 ||
-		      this.y < 0 || this.y > numHeight-1) {
+		if (this.x < 0 || this.x > numWidth*gridLen-gridLen ||
+		      this.y < 0 || this.y > numHeight*gridLen-gridLen) {
 			this.dead = true;
 		}
 	}
 }
 
-class Player extends Entity {
-	constructor(x, y, width, height, img) {
-		super(x, y, width, height, img);
+class Player extends AnimEntity {
+	constructor(x, y, width, height, img, nRow, nCol, frameSeq) {
+		super(x, y, width, height, img, nRow, nCol, frameSeq);
 
+		this.speed = 1;
 		this.facing = "right";
-
-		// Key presses
-		this.moveLeft = false;
-		this.moveRight = false;
-		this.moveUp = false;
-		this.moveDown = false;
 
 		this.shootTime = 20;
 		this.shootTimer = 0;
@@ -86,48 +110,42 @@ class Player extends Entity {
 	}
 
 	draw() {
-		super.draw();
-
+		super.drawAnimated(this.frameSeq);
 		if (this.facing == "left") {
-			ctx.translate(this.x*gridLen+gridLen, this.y*gridLen);
+			ctx.translate(this.x+gridLen, this.y);
 			ctx.scale(-1, 1);
 		} else if (this.facing == "right") {
-			ctx.translate(this.x*gridLen, this.y*gridLen);
+			ctx.translate(this.x, this.y);
 		} else if (this.facing == "up") {
-			ctx.translate(this.x*gridLen, this.y*gridLen+gridLen);
+			ctx.translate(this.x, this.y+gridLen);
 			ctx.rotate(-90*Math.PI/180);
 		} else if (this.facing == "down") {
-			ctx.translate(this.x*gridLen+gridLen, this.y*gridLen);
+			ctx.translate(this.x+gridLen, this.y);
 			ctx.rotate(90*Math.PI/180);
 		}
 
-		ctx.drawImage(gun, this.x, this.y);
+		ctx.drawImage(gun, 5, 5);
 		ctx.setTransform(1, 0, 0, 1, 0, 0);
 	}
 
 	// Update movement based on key presses
 	updateMovement() {
-		if (!this.moveLeft && Keys.left && this.x > 0) {
-			this.x--;
+		if (Keys.left && this.x > 0) {
+			this.x -= this.speed;
 			this.facing = "left";
 		}
-		if (!this.moveRight && Keys.right && this.x < numWidth-1) {
-			this.x++;
+		if (Keys.right && this.x < numWidth*gridLen-gridLen) {
+			this.x += this.speed;
 			this.facing = "right";
 		}
-		if (!this.moveUp && Keys.up && this.y > 0) {
-			this.y--;
+		if (Keys.up && this.y > 0) {
+			this.y -= this.speed;
 			this.facing = "up";
 		}
-		if (!this.moveDown && Keys.down && this.y < numHeight-1) {
-			this.y++;
+		if (Keys.down && this.y < numHeight*gridLen-gridLen) {
+			this.y += this.speed;
 			this.facing = "down";
 		}
-
-		this.moveLeft = Keys.left;
-		this.moveRight = Keys.right;
-		this.moveUp = Keys.up;
-		this.moveDown = Keys.down;
 	}
 
 	shoot() {
