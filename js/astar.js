183715e7c9bda6d7634f78ea72b77f385fbcc741
Gary1q2 <gary1q2@hotmail.com>
2020-02-12 03:13:06 +1100

    A* search actually works with more walls..    moved everything inside graph.js

--- js/astar.js
+++ js/astar.js
@@ -4,6 +4,27 @@ class Astar {
 		this.height = level.length;
 		this.width = level[0].length;
 		this.level = level;
+
+		this.start; 
+		this.goal;
+
+		// Find start and goal nodes from level array
+		for (var i = 0; i < this.level.length; i++) {
+			for (var j = 0; j < this.level[i].length; j++) {
+				if (this.level[i][j] == 2) {
+					this.start = {
+						x: j,
+						y: i
+					};
+				} else if (this.level[i][j] == 3) {
+					this.goal = {
+						x: j,
+						y: i
+					};
+				}
+			}
+		}
+
 		console.log("Loading in level, height="+this.height+", width="+this.width);
 
 		// Create 2D array
@@ -26,6 +47,7 @@ class Astar {
 		console.log(this.array);
 	}
 
+	// Checks if this position is a wall or not
 	isWall(pos) {
 		if (this.level[pos.y][[pos.x]] == 1) {
 			return true;
@@ -33,27 +55,29 @@ class Astar {
 		return false;
 	}
 
-	search(start, goal) {
+	// Start A* search
+	search() {
 
-		if (JSON.stringify(start) === JSON.stringify(goal)) {
+		// Disable goal to be same as start
+		if (JSON.stringify(this.start) === JSON.stringify(this.goal)) {
 			console.log("Already at the goal");
 			return;
 		}
 
 		// Initialise starting node in array
-		this.array[start.y][start.x].f = 0;
-		this.array[start.y][start.x].g = 0;
-		this.array[start.y][start.x].h = 0;
-		this.array[start.y][start.x].parent = null;
+		this.array[this.start.y][this.start.x].f = 0;
+		this.array[this.start.y][this.start.x].g = 0;
+		this.array[this.start.y][this.start.x].h = 0;
+		this.array[this.start.y][this.start.x].parent = null;
 
 		var openList = [];
 		var closedList = [];
-
-		openList.push(start);
+		openList.push(this.start);
 
 
 		while (openList.length != 0) {
 
+			// Find node with lowest f(x)
 			var lowIndex = 0;
 			for (var i = 0; i < openList.length; i++) {
 				if (openList[i].f < openList[lowIndex].f) {
@@ -63,9 +87,10 @@ class Astar {
 			var currentNode = openList[lowIndex];
 			console.log("Reached new node ["+currentNode.x+","+currentNode.y+"]");
 
-
+			// Move current node to closedList
 			openList.splice(lowIndex, 1);
 			closedList.push(currentNode);
+
 
 			var neighbours = this.neighbours(currentNode);
 			for (var i = 0; i < neighbours.length; i++) {
@@ -79,13 +104,14 @@ class Astar {
 
 
 				// Check if goal
-				if (JSON.stringify(neighbour) === JSON.stringify(goal)) {
+				if (JSON.stringify(neighbour) === JSON.stringify(this.goal)) {
 					this.array[neighbour.y][neighbour.x].parent = currentNode;
 					console.log("       Found goal");
 					return this.getPath(neighbour);
 
 				} else {
 					var newG = this.array[currentNode.y][currentNode.x].g + 1;
+
 
 					// Node in openList
 					if (this.containsObject(neighbour, openList)) {
@@ -104,13 +130,14 @@ class Astar {
 						if (this.array[neighbour.y][neighbour.x].g <= newG) {
 							continue;
 						}
-						//openList.push(neighbour);
-						//closedList.splice(closedList.indexOf(neighbour), 1);
+						openList.push(neighbour);
+						closedList.splice(closedList.indexOf(neighbour), 1);
 
+					// Add node to openList
 					} else {
 						console.log("       Not in any list... added to openList");
 						openList.push(neighbour);
-						this.array[neighbour.y][neighbour.x].h = this.manhattan(neighbour, goal);
+						this.array[neighbour.y][neighbour.x].h = this.manhattan(neighbour, this.goal);
 					}
 
 					this.array[neighbour.y][neighbour.x].g = newG;
@@ -124,13 +151,11 @@ class Astar {
 		return;
 	}
 
-	// Return the path from start to current node
+	// Return the path from start node to goal node
 	getPath(currNode) {
 		var curr = currNode
 		var path = [];
-		console.log("loop time");
 		while (this.array[curr.y][curr.x].parent) {
-			console.log("["+curr.x+","+curr.y+"]");
 			path.push(curr);
 			curr = this.array[curr.y][curr.x].parent;
 		}
@@ -143,7 +168,7 @@ class Astar {
 		return Math.abs(pos1.x-pos2.x) + Math.abs(pos1.y-pos2.y);
 	}
 
-	// Return all neighbours of the current node (only up, down, left & right)
+	// Return all valid neighbours of the current node (only up, down, left & right)
 	neighbours(pos) {
 		var list = [];
 
--- js/graph.js
+++ js/graph.js
@@ -5,28 +5,43 @@ class Graph {
 		this.level = level;
 
 		this.squareLen = 20;
+
+		this.astar = new Astar(level);
 	}
 
+	// Draw the maze with colors
 	drawMaze() {
 		for (var i = 0; i < this.height; i++) {
 			for (var j = 0; j < this.width; j++) {
 				if (this.level[i][j] == 0) {
 					ctx.fillStyle = "white";
 				} else if (this.level[i][j] == 1) {
-					ctx.fillStyle = "brown";
+					ctx.fillStyle = "black";
 				} else if (this.level[i][j] == 2) {
-					ctx.fillStyle = "blue";
+					ctx.fillStyle = "red";
 				} else if (this.level[i][j] == 3) {
-					ctx.fillStyle = "pink";
+					ctx.fillStyle = "blue";
 				} 
 				ctx.fillRect(j*this.squareLen, i*this.squareLen, this.squareLen, this.squareLen);
 			}
 		}
 	}
 
-	drawPath(path) {
+	// Draw the path on top of the maze
+	drawPath() {
+		var path = this.astar.search();
+
+		// Log the path
+		//console.log("\npath:");
+		for (var i = 0; i < path.length; i++) {
+			console.log("["+path[i].x+","+path[i].y+"]");
+		}
+
 		ctx.fillStyle = "green";
 		for (var i = 0; i < path.length; i++) {
+			if (JSON.stringify(path[i]) == JSON.stringify(this.astar.start) || JSON.stringify(path[i]) == JSON.stringify(this.astar.goal)) {
+				continue;
+			}
 			ctx.fillRect(path[i].x*this.squareLen, path[i].y*this.squareLen, this.squareLen, this.squareLen);
 		}
 	}
